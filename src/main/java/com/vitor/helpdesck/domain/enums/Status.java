72d3b8f8cc2a4c026dced3123c5bba1212f3f148
vitor277 <vitorsis2019@fef.edu.br>
2022-09-28 21:24:40 -0300

    tipos enumerados

---
+++ src/main/java/com/vitor/helpdesck/domain/enums/Status.java
@@ -0,0 +1,33 @@
+package com.vitor.helpdesck.domain.enums;
+
+public enum Status {
+    ABERTO(0, "ROLE_ABERTO"), ANDAMENTO(1, "ROLE_ANDAMENTO"), ENCERRADO(2, "ROLE_ENCERRADO");
+
+    private Integer codigo;
+    private String descricao;
+
+    Status(Integer codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Status toEnum(Integer cod){
+        if(cod == null){
+            return null;
+        }
+        for (Status x : Status.values()){
+            if(cod.equals(x.getCodigo())){
+                return x;
+            }
+        }
+        throw new IllegalArgumentException("Status Inválido");
+    }
+}
