254ebf309c074d8763ecf572eecb9a37979ab787
vitor277 <vitorsis2019@fef.edu.br>
2022-09-28 22:10:41 -0300

    classes de dominio iniciadas

---
+++ src/main/java/com/vitor/helpdesck/domain/Cliente.java
@@ -0,0 +1,20 @@
+package com.vitor.helpdesck.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente extends Pessoa{
+    private List<Chamado> chamados = new ArrayList<>();
+
+    public Cliente(Integer id, String nome, String cpf, String email, String senha, List<Chamado> chamados) {
+        super(id, nome, cpf, email, senha);
+    }
+
+    public List<Chamado> getChamados() {
+        return chamados;
+    }
+
+    public void setChamados(List<Chamado> chamados) {
+        this.chamados = chamados;
+    }
+}
---
+++ src/main/java/com/vitor/helpdesck/domain/Tecnico.java
@@ -0,0 +1,20 @@
+package com.vitor.helpdesck.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tecnico extends Pessoa{
+    private List<Chamado> chamados = new ArrayList<>();
+
+    public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
+        super(id, nome, cpf, email, senha);
+    }
+
+    public List<Chamado> getChamados() {
+        return chamados;
+    }
+
+    public void setChamados(List<Chamado> chamados) {
+        this.chamados = chamados;
+    }
+}
