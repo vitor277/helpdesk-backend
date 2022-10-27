76b2110b5871ec628f87839f17489b59eac59cb9
vitor277 <vitorsis2019@fef.edu.br>
2022-10-26 21:19:29 -0300

    Protegendo aplicação contra serialização

---
+++ src/main/java/com/vitor/helpdesck/services/TecnicoService.java
@@ -0,0 +1,19 @@
+package com.vitor.helpdesck.services;
+
+import com.vitor.helpdesck.domain.Tecnico;
+import com.vitor.helpdesck.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+    @Autowired
+    private TecnicoRepository repository;
+
+    public Tecnico findById(Integer id){
+        Optional<Tecnico> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
